d73fe6754b96b8ace0f9a3ef5a6a9691f055cf59
abdelhadia72 <thisisavnb@gmail.com>
2024-04-24 16:24:01 +0100

    trigger

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,8 @@
+CREATE TRIGGER decrease_items_quantity
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+END;
