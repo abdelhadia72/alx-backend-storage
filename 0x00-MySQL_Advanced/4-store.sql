ce4cd0413114c05eee01edd2b82aab83c1517a8d
abdelhadia72 <thisisavnb@gmail.com>
2024-04-24 17:02:17 +0100

    update commend

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,3 +1,7 @@
+-- Decreases the quantity of items in response to new orders
+-- Updates the 'quantity' of items by subtracting the newly inserted 'number'
+-- Targets the item specified by 'item_name' in the 'items' table
+
 CREATE TRIGGER decrease_items_quantity
 AFTER INSERT ON orders
 FOR EACH ROW
