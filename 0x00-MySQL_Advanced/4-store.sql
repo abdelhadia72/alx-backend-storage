8f72242ad3090b20e9d5cb5a391f870e1d718454
abdelhadia72 <thisisavnb@gmail.com>
2024-04-25 16:44:14 +0100

    fixing task 3

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -2,11 +2,5 @@
 -- Updates the 'quantity' of items by subtracting the newly inserted 'number'
 -- Targets the item specified by 'item_name' in the 'items' table
 
-CREATE TRIGGER decrease_items_quantity
-AFTER INSERT ON orders
-FOR EACH ROW
-BEGIN
-    UPDATE items
-    SET quantity = quantity - NEW.number
-    WHERE name = NEW.item_name;
-END;
+CREATE TRIGGER decrease_items_quantity AFTER INSERT ON orders FOR EACH ROW
+UPDATE items SET quantity = quantity - NEW.number WHERE name=NEW.item_name;
