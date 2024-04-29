5f6753763b58c0e69f87cc3bbc28dd97141ad397
abdelhadia72 <thisisavnb@gmail.com>
2024-04-29 19:37:51 +0100

    looping on all doc and print them

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+    lists all documents in a collection
+"""
+
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+database = client.my_db
+collection = database.school
+
+for doc in collection.find():
+    """
+    function loops on all documents and print them
+    """
+    print(doc)
