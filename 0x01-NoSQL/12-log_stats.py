ea087b8f921bdf881ccc8d51c04e99ba791702e0
abdelhadia72 <thisisavnb@gmail.com>
2024-04-30 18:58:54 +0100

    fix pycodestyle and chagne to formant and add main to main

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,18 +5,22 @@ from pymongo import MongoClient
 
 
 def log_stats():
-    """ Python script that provides some stats about Nginx logs stored in MongoDB """
+    """
+        Python script that provides some
+        stats about Nginx logs stored in MongoDB
+    """
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
 
-    print(f"{nginx_collection.count_documents({})} logs")
+    print("{} logs".format(nginx_collection.count_documents({})))
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     print("Methods:")
     for method in methods:
-        print(f"\tmethod {method}: {
-              nginx_collection.count_documents({"method": method})}")
-    print(f"{nginx_collection.count_documents(
-        {"path": "/status"})} status check")
+        count_method = nginx_collection.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, count_method))
+    path_status = nginx_collection.count_documents({"path": "/status"})
+    print("{} status check".format(path_status))
 
 
-log_stats()
+if __name__ == "__main__":
+    log_stats()
