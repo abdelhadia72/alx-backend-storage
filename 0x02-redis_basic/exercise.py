e500592fce0ea0ca11f6f8f690fe2a436da2b858
abdelhadia72 <thisisavnb@gmail.com>
2024-05-01 15:14:19 +0100

    Let's build a Teacher interface

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -11,6 +11,7 @@ class Cache:
     """
         class for cache
     """
+
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
@@ -25,3 +26,31 @@ class Cache:
         else:
             raise TypeError("Data type not supported")
         return key
+
+    def get(self,
+            key: str,
+            fn: callable = None) -> Union[str,
+                                          bytes,
+                                          int,
+                                          float]:
+        """
+            get method for cache
+        """
+        value = self._redis.get(key)
+        if key is None:
+            return None
+        if fn is not None:
+            return fn(value)
+        return value
+
+    def get_str(self):
+        """
+            get method for cache
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self):
+        """
+            get method for cache
+        """
+        return self.get(key, fn=int)
